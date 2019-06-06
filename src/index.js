f53ab531bf5d813c327bd5669ec4d9382fff0402
Дмитрий <dm_lis@mail.ru>
2019-06-06 09:48:24 +0300

    Example Event Handler

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    clickOnMe(event) {
+        console.log('clicked on ', event.target);
+    }
     render() {
         return (
             <div>
-                <h1>React initialized</h1>
+                <h1>Event Handler</h1>
+                <button onClick={this.clickOnMe}>{ this.props.children }</button>
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <App />,
+    <App>Click Me!</App>,
     document.getElementById('app')
 );
