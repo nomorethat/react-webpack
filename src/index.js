bc8fb316a50d6a9e9a17712f4f735b3ccf876566
Дмитрий <dm_lis@mail.ru>
2019-09-04 12:03:08 +0300

    Install PropTypes, передача свойств в компонент с валидацией и значениями по умолчанию, если свойство не было передано.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
+
+    btnOnClick(event) {
+        // console.log(event.target);
+    }
+
     render() {
         return (
             <div>
-                <h1>React initialized</h1>
+                <h1>{ this.props.h1Text }</h1>
+                <button onClick={this.btnOnClick}>{ this.props.btnText }</button>
             </div>
         );
     }
 }
 
+App.propTypes = {
+    btnText: PropTypes.string,
+    h1Text: PropTypes.string.isRequired
+};
+
+App.defaultProps = {
+    btnText: 'Default Click'
+};
+
 ReactDOM.render(
-    <App />,
+    <App h1Text={'Example props with validation and default'}/>,
     document.getElementById('app')
 );
